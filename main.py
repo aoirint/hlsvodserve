4684b5a39a037215a47ca62912025d6b0352dcf9
aoirint <aoirint@gmail.com>
2022-06-06 16:11:15 +0900

    use asyncio

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from pathlib import Path
-import subprocess
-from threading import Thread
+import asyncio
+from asyncio.subprocess import create_subprocess_exec, PIPE
+from concurrent.futures import ThreadPoolExecutor
+import re
+import tempfile
+import platform
 
-def main(
+# https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+if platform.system() == 'Windows':
+  asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+async def main(
   video_file: str,
   outdir: str,
 ):
@@ -39,26 +47,40 @@ def main(
     str(hls_playlist_filename),
   ]
 
-  proc = subprocess.Popen(
-    command,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
+  proc = await create_subprocess_exec(
+    command[0],
+    *command[1:],
+    stdout=PIPE,
+    stderr=PIPE,
   )
 
+  loop = asyncio.get_event_loop()
+  executor = ThreadPoolExecutor()
+
   def read_stdout(stdout):
-    for line in stdout:
+    while True:
+      line = asyncio.run_coroutine_threadsafe(stdout.readline(), loop).result()
+      if not line:
+        break
       line_text = line.decode('utf-8').strip()
       print(f'STDOUT: {line_text}', flush=True)
     print('stdout closed') # closed when process exited
 
   def read_stderr(stderr):
-    for line in stderr:
+    while True:
+      line = asyncio.run_coroutine_threadsafe(stderr.readline(), loop).result()
+      if not line:
+        break
       line_text = line.decode('utf-8').strip()
       print(f'STDERR: {line_text}', flush=True)
     print('stderr closed') # closed when process exited
 
-  Thread(target=read_stdout, args=(proc.stdout,)).start()
-  Thread(target=read_stderr, args=(proc.stderr,)).start()
+  loop.run_in_executor(executor, read_stdout, proc.stdout)
+  loop.run_in_executor(executor, read_stderr, proc.stderr)
+
+  await proc.wait()
+
+  print('exited')
 
 if __name__ == '__main__':
   import argparse
@@ -70,7 +92,7 @@ if __name__ == '__main__':
   video_file = args.video_file
   outdir = args.outdir
 
-  main(
+  asyncio.run(main(
     video_file=video_file,
     outdir=outdir,
-  )
+  ))
